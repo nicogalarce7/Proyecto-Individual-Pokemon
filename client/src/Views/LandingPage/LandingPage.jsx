a3923ba7ca5d7fca905ec73134538b6314fbcd76
NicoGalarce1 <nicolasgalarce18@gmail.com>
2023-02-17 15:36:06 +0100

    100%

--- client/src/Views/LandingPage/LandingPage.jsx
+++ client/src/Views/LandingPage/LandingPage.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import "./LandingPage.css";
 import { NavLink } from "react-router-dom";
 import flecha from "../../assets/flecha.png";
-
+import Press from "../../assets/press.png"
+import title from "../../assets/Pokemon_logo.png"
+import ProyectoNico from "../../assets/poyectoNico.png"
 export default function Landingpage() {
   return (
     <>
       <div className="container_landing">
-        <div className="flecha">
-        <img className="img_fle" src={flecha} alt="flecha" />  
+        <img className= "ttile_L"src={title} alt="title" />
+         <img className="preess" src={Press} alt="press"/>
+        <img className="img_fle" src={flecha} alt="flecha" /> 
         <div className="bar">
           <NavLink to="/home" className="container_button">
             <span>0</span>
           </NavLink>
-        </div>
-        </div>
+        </div> 
+        <img className="title_name" src={ProyectoNico} alt="name" />
       </div>
     </>
   );
