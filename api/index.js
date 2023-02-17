e3e59449ac08f8bdbd7ff57bd437e901e0f97b88
NicoGalarce1 <nicolasgalarce18@gmail.com>
2023-02-17 01:51:21 +0100

    100%

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 console.clear();
+require('dotenv').config();
+const {PORT} = process.env;
 const server = require('./src/app.js');
 const { conn, } = require('./src/db.js');
 
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at',PORT); // eslint-disable-line no-console
   });
 });
 
